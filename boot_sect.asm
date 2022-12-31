987b17aa04a685cae081fdf820bd529c230a2664
Dielson Sales <dielsonscarvalho@gmail.com>
2022-12-31 12:07:40 -0300

    Instruct boot sector to print "Hello World"

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,6 +1,32 @@
-loop:
-	jmp loop
+; A simple boot sector that prints "Hello World"
 
-times 510-($-$$) db 0
+mov ah, 0x0e ; Teletype BIOS routine
 
-dw 0xaa55
+mov al, 'H'
+int 0x10 ; Interrupt instruction
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+
+jmp $ ; Jump to the current address forever
+
+times 510-($-$$) db 0 ; Pad the boot sector with zeroes
+
+dw 0xaa55 ; Lets BIOS know this is a boot sector
