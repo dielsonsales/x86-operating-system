b1b9badadc6330c6fc3108a9d328c1cf19b48b32
Dielson Sales <dielsonscarvalho@gmail.com>
2023-01-04 16:30:45 -0300

    Print hexadecimal values as ascii characters

--- boot_sect.asm
+++ boot_sect.asm
@@ -3,14 +3,14 @@
 [org 0x7c00] ; Tell the assembler where this code will be loaded
              ; Automatically offsets the addresses with this value
 
-mov bx, HELLO_MSG ; bx = &HELLO_MSG
-call print_string
+; mov bx, HELLO_MSG ; bx = &HELLO_MSG
+; call print_string
 
-mov bx, LINE_BREAK ; bx = &LINE_BREAK
-call print_string
+; mov bx, GOODBYE_MSG ; bx = &GOODBYE_MSG
+; call print_string
 
-mov bx, GOODBYE_MSG ; bx = &GOODBYE_MSG
-call print_string
+mov dx, 0x1fb6 ; 0001 1111 1011 0110
+call print_hex
 
 jmp $ ; Halts forever
 
@@ -25,5 +25,12 @@ LINE_BREAK:
 GOODBYE_MSG:
     db 'Goodbye', 0
 
+HEX_OUT:
+    db '0x0000', 0
+
 times 510-($-$$) db 0 ; Pads the boot sector with zeroes and adds BIOS bootable number
 dw 0xaa55 ; Lets BIOS know this is a boot sector
+
+; 0001 1111 1011 0110 >> 8
+; 0000 0000 0001 1111
+; 
--- print_string.asm
+++ print_string.asm
@@ -9,5 +9,49 @@ print_compare:
     int 0x10 ; print(al)
     jmp print_compare
 return_print_string:
-    popa
+    popa ; Pops all registers back
     ret
+
+print_hex:
+    pusha ; Pushes all registers to the stack
+    mov ah, 0x0e ; Bios tele-type output
+    mov al, '0' ; al = '0'
+    int 0x10 ; print(al)
+    mov al, 'x' ; al = 'x'
+    int 0x10 ; print(al)
+
+    mov word bx, dx ; bx = dx
+    shr bx, 12
+    call ascii_value
+
+    mov word bx, dx ; bx = dx
+    shr bx, 8
+    call ascii_value
+
+    mov word bx, dx ; bx = dx
+    shr bx, 4
+    call ascii_value
+
+    mov word bx, dx ; bx = dx
+    call ascii_value
+    popa ; Pops all registers back
+    ret
+
+ascii_value:
+    and bx, 0x0F ; removes the first 8 bits
+    cmp bl, 0x09
+    jle ascii_for_number
+    jmp ascii_for_letter
+print_ascii_value:
+    mov al, bl ; al = bl
+    int 0x10 ; print(al)
+    ret
+
+ascii_for_number:
+    add bx, 0x0030
+    jmp print_ascii_value
+
+ascii_for_letter:
+    add bx, 0x0037
+    jmp print_ascii_value
+
